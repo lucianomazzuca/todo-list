b49ceb7465f7d6303ab06824058b63215e120d9e
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2020-10-07 00:59:13 -0300

    Added task render

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -1,16 +1,20 @@
 import dbProjects from "./projectsController";
 
-const event = (function() {
+const event = function() {
     //Cache DOM
     const popupBg = document.querySelector('#project-form');
     const btnAddProject = document.getElementById('add-project');
     const projectContainer = document.getElementsByClassName('project-container');
     const btnSubmitProject = document.querySelector('#sumbmit-project');
+    const projectLink = document.querySelectorAll('.project');
 
     //Bind Events
     btnAddProject.addEventListener('click', openFormAddProject);
-    btnSubmitProject.addEventListener('click', getNewProjectName)
+    btnSubmitProject.addEventListener('click', getNewProjectName);
     
+    projectLink.forEach(project =>{
+        project.addEventListener('click', renderTasks);
+    })
 
     function openPopupBg() {
         popupBg.style.display = 'flex';
@@ -33,23 +37,82 @@ const event = (function() {
         closePopupBg()
     }
 
+    function getTasks(e) {
+        let projectIndex = e.target.id;
+        let tasks = dbProjects.getTaskFromProject(projectIndex);
+        
+        console.log(tasks)
+        return tasks
+    }
+
+    function deleteTaskContainer() {
+        const elements = document.getElementsByClassName("task-card");
+        while (elements.length > 0) {
+            elements[0].parentNode.removeChild(elements[0]);
+        }
+    }
+
+    function renderTasks(e) {
+        const tasks = getTasks(e);
+        deleteTaskContainer();
+
+        tasks.forEach(function(task, index){
+            //Render card
+            const divTask = document.createElement('div');
+            divTask.classList.add('task-card');
+            divTask.id = index;
+            document.querySelector('.task-container').appendChild(divTask);
+
+            //Title
+            const title = document.createElement('div');
+            title.classList.add('title');
+            title.innerText = task.name;
+            divTask.appendChild(title);
+
+            //Expire
+            const date = document.createElement('div');
+            date.classList.add('expire');
+            date.innerText = task.dueDate;
+            divTask.appendChild(date);
+
+            //Button
+            const btnDetail = document.createElement('button');
+            btnDetail.innerText = 'Details';
+            const divDetail = document.createElement('div');
+            divDetail.classList.add('task-details');
+            divDetail.appendChild(btnDetail);
+            divTask.appendChild(divDetail)
+
+            const btnDelete = document.createElement('button');
+            btnDelete.innerText = 'Delete';
+            const divDelete = document.createElement('div');
+            divDelete.classList.add('task-delete');
+            divDelete.appendChild(btnDelete);
+            divTask.appendChild(divDelete);
+        })
+
+    }
+
     return{
         getNewProjectName
     }
-})()
+}
 
 
 function renderProjectsSidebar() {
     let projects = dbProjects.getAllProjects();
 
-    for(let project of projects){
+    projects.forEach(function(project, index){
         const divProject = document.createElement('div');
         divProject.classList.add('project');
+        divProject.id = index;
         divProject.innerText = project.name;
         document.querySelector('.project-container').appendChild(divProject)
-    }
+    })
 }
 
+
 renderProjectsSidebar();
+event()
 
 export default event
--- src/modules/projectsController.js
+++ src/modules/projectsController.js
@@ -73,10 +73,12 @@ let project2 = new Project('Martes');
 
 let task1 = new Task('ir al gimnasio', 'Hacer pecho y hombros', 'media', '10/10/2020');
 let task2 = new Task('Preparar la cena', 'Hacer pecho y hombros', 'media', '10/10/2020');
+let task3 = new Task('Armar la cama', 'Hacer pecho y hombros', 'media', '10/10/2020');
 
 dbProjects.addProject(project1)
 dbProjects.addTask(0, task1);
 dbProjects.addTask(0, task2);
+dbProjects.addTask(0, task3);
 
 dbProjects.addProject(project2)
 dbProjects.addTask(1, task1);
